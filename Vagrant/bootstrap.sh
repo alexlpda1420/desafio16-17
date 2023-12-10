b272d5da29cbd1f632e53d40bda0063b1e6da3c8
Alexis Roldan <a.eroldan@hotmail.com>
2023-12-10 13:25:06 -0300

    Pruebas de todo

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -15,3 +15,13 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 #Install minikube  
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+#Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#Install ArgoCD
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
